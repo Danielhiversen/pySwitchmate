228f6fb515d1d96cb8b00e649f138988aaf176be
qiz-li <47799695+qiz-li@users.noreply.github.com>
2022-07-13 10:05:40 -0700

    Move from `bluepy` to `bleak` (#12)
    * Move from `bluepy` to `bleak`
    
    * Persist connection between central and Switchmate
    
    Add back `_connect` method
    
    * Await disconnection
    
    Forgot to await to disconnect.
    Forgot brackets to call method.
    
    * Determine `handle` based on device model
    
    The handle varies depending if the device is the "Light" or "Bright" model.
    The library was using the "Light" handle.
    It works to control "Bright" models. However, manually toggling the switch does not update the handle state.
    This addresses and should hopefully resolve Issue #9.
    
    * Update structure
    
    Combine `update` and `_sendpacket` into `_communicate` method.
    
    * Add `bool` return for all methods
    
    Greater consistency throughout the class.
    
    * Lock `_connect` and `_disconnect` separately
    
    * Bump version and dependency
    
    * Resolve Hound warning
    
    * Format with Black
    
    * Line length under 80

--- switchmate/__init__.py
+++ switchmate/__init__.py
@@ -1,12 +1,14 @@
 """Library to handle connection with Switchmate"""
 
+import asyncio
 import logging
 
-import bluepy
+import bleak
 
-HANDLE = 0x2e
-ON_KEY = b'\x01'
-OFF_KEY = b'\x00'
+CONNECT_LOCK = asyncio.Lock()
+
+ON_KEY = b"\x01"
+OFF_KEY = b"\x00"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,59 +22,87 @@ class Switchmate:
         self._device = None
         self.available = False
         self._flip_on_off = flip_on_off
+        self._handle = None
 
-    def _connect(self) -> bool:
+    async def _connect(self) -> bool:
+        # Disconnect before reconnecting
         if self._device is not None:
-            _LOGGER.debug("Disconnecting")
-            try:
-                self._device.disconnect()
-            except bluepy.btle.BTLEException:
-                pass
+            await self._disconnect()
+        _LOGGER.debug("Connecting")
+        self._device = bleak.BleakClient(self._mac)
         try:
-            _LOGGER.debug("Connecting")
-            self._device = bluepy.btle.Peripheral(self._mac,
-                                                  bluepy.btle.ADDR_TYPE_RANDOM)
-        except (bluepy.btle.BTLEException, BrokenPipeError):
-            _LOGGER.error("Failed to connect to switchmate",
-                          exc_info=logging.DEBUG >= _LOGGER.root.level)
+            async with CONNECT_LOCK:
+                await self._device.connect()
+                if self._handle is None:
+                    # Determine handle based on Switchmate model
+                    self._handle = (
+                        47
+                        if await self._device.read_gatt_char(21) == b"Bright"
+                        else 45
+                    )
+        except (bleak.BleakError, asyncio.exceptions.TimeoutError):
+            _LOGGER.error(
+                "Failed to connect to Switchmate",
+                exc_info=logging.DEBUG >= _LOGGER.root.level,
+            )
+            return False
+        return True
+
+    async def _disconnect(self) -> bool:
+        _LOGGER.debug("Disconnecting")
+        try:
+            async with CONNECT_LOCK:
+                await self._device.disconnect()
+        except (bleak.BleakError, asyncio.exceptions.TimeoutError):
+            _LOGGER.error(
+                "Failed to disconnect from Switchmate",
+                exc_info=logging.DEBUG >= _LOGGER.root.level,
+            )
+            return False
+        return True
+
+    async def _communicate(self, key=None, retry=True) -> bool:
+        try:
+            if (
+                self._device is None or not self._device.is_connected
+            ) and not await self._connect():
+                raise bleak.BleakError("No connection to Switchmate")
+            async with CONNECT_LOCK:
+                if key:
+                    _LOGGER.debug("Sending key %s", key)
+                    await self._device.write_gatt_char(self._handle, key, True)
+                else:
+                    _LOGGER.debug("Updating Switchmate state")
+                    self.state = (
+                        await self._device.read_gatt_char(self._handle)
+                        == ON_KEY
+                        if not self._flip_on_off
+                        else OFF_KEY
+                    )
+        except (bleak.BleakError, asyncio.exceptions.TimeoutError):
+            if retry:
+                return await self._communicate(key, False)
+            _LOGGER.error(
+                "Cannot communicate with Switchmate",
+                exc_info=logging.DEBUG >= _LOGGER.root.level,
+            )
             self.available = False
             return False
         self.available = True
         return True
 
-    def _sendpacket(self, key, retry=2) -> bool:
-        try:
-            _LOGGER.debug("Sending key %s", key)
-            self._device.writeCharacteristic(HANDLE, key, True)
-        except (bluepy.btle.BTLEException, BrokenPipeError):
-            if retry < 1 or not self._connect():
-                _LOGGER.error("Cannot connect to switchmate.",
-                              exc_info=logging.DEBUG >= _LOGGER.root.level)
-                self.available = False
-                return False
-            return self._sendpacket(key, retry-1)
-        self.available = True
-        return True
-
-    def update(self, retry=2) -> None:
+    async def update(self) -> bool:
         """Synchronize state with switch."""
-        try:
-            _LOGGER.debug("Updating device state.")
-            key = ON_KEY if not self._flip_on_off else OFF_KEY
-            self.state = self._device.readCharacteristic(HANDLE) == key
-        except (bluepy.btle.BTLEException, AttributeError):
-            if retry < 1 or not self._connect():
-                self.available = False
-                _LOGGER.error("Failed to update device state.", exc_info=True)
-                return None
-            return self.update(retry-1)
-        self.available = True
-        return None
+        return await self._communicate()
 
-    def turn_on(self) -> bool:
+    async def turn_on(self) -> bool:
         """Turn the switch on."""
-        return self._sendpacket(ON_KEY if not self._flip_on_off else OFF_KEY)
+        return await self._communicate(
+            ON_KEY if not self._flip_on_off else OFF_KEY
+        )
 
-    def turn_off(self) -> bool:
+    async def turn_off(self) -> bool:
         """Turn the switch off."""
-        return self._sendpacket(OFF_KEY if not self._flip_on_off else ON_KEY)
+        return await self._communicate(
+            OFF_KEY if not self._flip_on_off else ON_KEY
+        )
